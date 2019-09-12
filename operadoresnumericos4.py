18a572b537d55171b052abd3f3f4b4ff806bf7e7
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:35:16 -0400

    Add files via upload

---
+++ operadoresnumericos4.py
@@ -0,0 +1,25 @@
+# Proposito: Trabajar con los operadores aritmrtico 
+
+# Entrada de datos
+a = float(input('ingrese el valor A: '))
+
+b = float(input('ingrese el valor B: '))
+
+c = float(input('ingrese el valor C: '))
+
+d = float(input('ingrese el valor D: '))
+
+e = float(input('ingrese el valor E: '))
+
+f = float(input('Ingrese el valor F: '))
+
+#Prosedimiento
+
+z = a + (((b)/(c))/(d - (e)/(f))) 
+
+#Salida del programa
+
+print('La respuesta de la operacion A+(((B)/(C))/(D-(E)/(F))) es: ',z)
+
+# Fin del programa
+input('presione cualquier tecla para continuar...')#pausa
